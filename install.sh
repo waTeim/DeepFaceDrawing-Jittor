1f7aa247c816a1dcc81ee85f81801dfc20bb8ca6
waTeim <truthset@gmail.com>
2020-09-10 18:06:19 -0400

    Several problem fixes

--- install.sh
+++ install.sh
@@ -1,6 +1,25 @@
 # install necessary library
 pip install pyqt5
-pip install Pillow 
-pip install scipy 
-pip install dominate 
+pip install Pillow
+pip install scipy
+pip install dominate
+
+# On many systems Qt for the desktop does not correspond to the
+# libraries that OpenCV was compiled against.  
+#
+# In particular those libraries are
+#
+# libQtDBus
+# libQtCore
+# libQtGui
+#
+# This will cause a segmentation fault when the program is run
+# 
+# see https://github.com/skvark/opencv-python/issues/46
+#
+# In that case, uncomment the following to force pip to build opencv-python
+# from source.
+#
+# pip install --no-binary opencv-python opencv-python
+#
 pip install opencv-python
